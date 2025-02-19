0e0ce828147637f03ef79faa73ac0d9df1b45b1b
baiwusanyu-c <baiwusanyu-c@users.noreply.github.com>
2025-02-19 09:52:34 +0000

    deploy: 44b48980d2ad41bf5586c038bc97f1b9b84c707e

--- assets/index-ac316c38.js
+++
@@ -1,2 +0,0 @@
-import{_ as c,o as s,e as o,z as _,A as t,f as a}from"./index-4e8621ec.js";const d={},n=e=>(_("data-v-3a58cc77"),e=e(),t(),e),p={class:"app-container"},i=n(()=>a("div",{class:"welcome f-c-c h100%"},"欢迎您！",-1)),r=[i];function l(e,f){return s(),o("div",p,r)}const u=c(d,[["render",l],["__scopeId","data-v-3a58cc77"]]);export{u as default};
-//# sourceMappingURL=index-ac316c38.js.map
--- assets/user-64375c57.js
+++
@@ -1,2 +0,0 @@
-import{G as e}from"./index-4e8621ec.js";const s=()=>e({url:"/user/login",method:"get"}),o=t=>e({url:"/user/list",method:"post",data:t}),u=t=>e({url:"/user/add",method:"post",data:t}),d=t=>e({url:"/user/edit",method:"post",data:t});export{o as a,u as b,d as e,s as g};
-//# sourceMappingURL=user-64375c57.js.map
